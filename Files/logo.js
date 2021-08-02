f3f22fc4f7c9cd397c58ca37296480c218005b98
PawelWlasow <50367039+PawelWlasow@users.noreply.github.com>
2021-08-02 23:41:59 +0300

    fourthWeek

--- Files/logo.js
+++ Files/logo.js
@@ -4,8 +4,8 @@ const flask = document.createElement ('img');
 flask.src = 'images/flask1.png';
 logo.appendChild (flask);
 window.onload = function () {
-  var index = 0;
-  var id = 'letterNumber';
+  let index = 0;
+  let id = 'letterNumber';
   for (i = 1; i <= 7; i++) {
     const letter = document.createElement ('span');
     logo.appendChild (letter).id = id + i;
